3ee5e662dd9f540c105ea8cfad1c6d08fbe0d23c
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-11-21 12:42:29 +0200

    final-corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from './Profile/user.json';
 import stats from './Statistics/data.json';
 import friends from './FriendList/friends.json';
-console.log(user);
+import transactions from './TransactionHistory/transactions.json';
+
 export const App = () => {
   return (
     <>
@@ -18,6 +20,7 @@ export const App = () => {
       <Statistics title="upload stats" stats={stats} />
       <Statistics stats={stats} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,10 +1,11 @@
 import s from './FriendList.module.css';
 import PropTypes from 'prop-types';
+import defaultAvatar from './defaultAvatar.jpg';
 
 export default function FriendList({ friends }) {
   return (
     <ul className={s.friendlist}>
-      {friends.map(({ avatar, name, isOnline, id }) => (
+      {friends.map(({ avatar = defaultAvatar, name, isOnline, id }) => (
         <li key={id} className={s.item}>
           <span className={isOnline ? s.isOnline : s.isOffline}></span>
           <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
